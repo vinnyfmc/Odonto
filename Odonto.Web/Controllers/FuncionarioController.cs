fa97cf1280c55faf884a56c6f73f8134c0e7a860
Vinicius Amaral <vinnyfmc@gmail.com>
2018-03-11 22:49:52 -0300

    Funcionario e Paciente - Index e controller

---
+++ Odonto.Web/Controllers/FuncionarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Odonto.Web.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
